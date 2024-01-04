b5f52d0ee723732694fa775f143f122b511fc385
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-04 18:56:58 +0100

    Add routes for quiz components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { QuizInfoComponent } from './pages/quiz-info/quiz-info.component';
+import { QuizJoinComponent } from './pages/quiz-join/quiz-join.component';
+import { QuizScoreComponent } from './pages/quiz-score/quiz-score.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: QuizJoinComponent },
+  { path: 'quiz-info', component: QuizInfoComponent },
+  { path: 'quiz-score', component: QuizScoreComponent },
+];
---
+++ src/app/pages/quiz-score/quiz-score.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-score',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-score.component.html',
+  styleUrl: './quiz-score.component.css'
+})
+export class QuizScoreComponent {
+
+}
