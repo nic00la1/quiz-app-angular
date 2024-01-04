7219101d77c0ec178f9822ec98dc12fde703b9a1
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-04 19:03:50 +0100

    Refactor routes and remove unnecessary code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { QuizInfoComponent } from './pages/quiz-info/quiz-info.component';
 import { QuizJoinComponent } from './pages/quiz-join/quiz-join.component';
 import { QuizScoreComponent } from './pages/quiz-score/quiz-score.component';
 import { QuizComponent } from './pages/quiz/quiz.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: QuizJoinComponent },
   { path: 'quiz-info', component: QuizInfoComponent },
   { path: 'quiz', component: QuizComponent },
   { path: 'quiz-score', component: QuizScoreComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
