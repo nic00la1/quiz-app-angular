3f998f3614ad17ceda4cacbf8a5cee8fa6bb3bf7
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-04 18:54:45 +0100

    Refactor code to improve performance and readability

---
+++ src/app/pages/quiz-join/quiz-join.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-join',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-join.component.html',
+  styleUrl: './quiz-join.component.css'
+})
+export class QuizJoinComponent {
+
+}
