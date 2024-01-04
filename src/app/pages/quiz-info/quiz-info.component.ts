f47767e9e3eaa4d5ee50c17e32b83888b006a600
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-04 18:55:20 +0100

    Fix bug in login functionality

---
+++ src/app/pages/quiz-info/quiz-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-info.component.html',
+  styleUrl: './quiz-info.component.css'
+})
+export class QuizInfoComponent {
+
+}
