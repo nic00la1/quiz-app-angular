745184d475339f9463acf50aa0b1c482155f1fda
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-04 19:24:42 +0100

    Refactor quiz-join component to include code and name inputs
    This commit refactors the quiz-join component to include code and name inputs. It also adds two-way data binding using ngModel for the inputs. Additionally, a join() method is implemented to handle the join button click event. The method checks if both code and name inputs are filled, and logs them to the console if they are. Otherwise, an alert is displayed prompting the user to enter a code and a name.

--- src/app/pages/quiz-join/quiz-join.component.ts
+++ src/app/pages/quiz-join/quiz-join.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz-join',
   standalone: true,
-  imports: [MatInputModule, MatButtonModule],
+  imports: [MatInputModule, MatButtonModule, FormsModule],
   templateUrl: './quiz-join.component.html',
   styleUrl: './quiz-join.component.css',
 })
-export class QuizJoinComponent {}
+export class QuizJoinComponent {
+  code!: string;
+  name!: string;
+
+  join() {
+    if (this.code && this.name) {
+      console.log(this.code, this.name);
+    } else {
+      alert('Please enter a code and a name');
+    }
+  }
+}
