e7ffd0864afdc7b845a805772dcb278bd5805a51
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-05 07:46:03 +0100

    Add router navigation if quiz result is not available

--- src/app/pages/quiz-info/quiz-info.component.ts
+++ src/app/pages/quiz-info/quiz-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { TestService } from '../../services/test.service';
 import { Quiz } from '../../types';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-info',
@@ -12,9 +13,14 @@ import { Quiz } from '../../types';
 export class QuizInfoComponent implements OnInit {
   testService = inject(TestService);
   quizInfo!: Quiz;
+  router = inject(Router);
 
   ngOnInit(): void {
     let quizResult = this.testService.quizResult;
+    if (!quizResult) {
+      this.router.navigateByUrl('/');
+      return;
+    }
     let quizId = quizResult.quizId;
     this.testService.getQuizById(quizId).subscribe((quiz) => {
       this.quizInfo = quiz;
